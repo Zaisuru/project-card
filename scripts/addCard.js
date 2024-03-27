ed8448a7c5094ddbcb3017c026bbad4cb4878b46
Thomas HOLDERBAUM <thomas.holderbaum@regnology.net>
2024-03-27 10:31:58 +0100

    add JS

--- scripts/addCard.js
+++ scripts/addCard.js
@@ -1,18 +1,44 @@
-    
-    const cardElement = document.createElement("div");
-    const cardImgElement = document.createElement("div");
-    const cardBodyElement = document.createElement("div");
-    const cardTitleElement = document.createElement("h3");
-    const cardTextElement = document.createElement("p");
+createNewProjectCard= (link,title,text) => {
+    const card = document.createElement("div")
+    card.classList.add("card")
 
-    cardElement.classList.add("card");
-    cardImgElement.classList.add("card-img-top");
-    cardBodyElement.classList.add("card-body");
-    cardTitleElement.classList.add("card-title");
-    cardTextElement.classList.add("card-text");
+    const cardImg = document.createElement("img")
+    cardImg.classList.add("card-img-top")
+    cardImg.src = link
+    cardImg.alt = ''
+    card.appendChild(cardImg)
 
-    document.getElementById("projectCard").appendChild(cardElement);
-    cardElement.appendChild(cardImgElement);
-    cardElement.appendChild(cardBodyElement);
-    cardBodyElement.appendChild(cardTitleElement);
-    cardBodyElement.appendChild(cardTextElement);
+    const cardBody = document.createElement("div")
+    cardBody.classList.add("card-body")
+    card.appendChild(cardBody)
+
+    const cardTitle = document.createElement("h3")
+    cardTitle.classList.add("card-title")
+    cardTitle.innerText = title
+    cardBody.appendChild(cardTitle)
+
+    const cardText = document.createElement("p")
+    cardText.classList.add("card-text")
+    cardText.innerText=text
+    cardBody.appendChild(cardText)
+}
+
+const resetForm = () =>{
+    document.getElementsByClassName("addCard__title_value").value = ''
+    document.getElementsByClassName("addCard__pictures_value").value = ''
+    document.getElementsByClassName("addCard__text_value").value =''
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const formTitle = document.getElementsByClassName("addCard__title_value").value;
+    const formLink = document.getElementsByClassName("addCard__pictures_value").value;
+    const formText = document.getElementsByClassName("addCard__text_value").value
+
+    const newProjectCard = createNewProjectCard(formLink,formTitle,formText);
+
+    document.getElementById("formCard").appendChild(newProjectCard);
+
+    resetForm()
+}
